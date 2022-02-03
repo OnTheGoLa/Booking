82de518e78ca07f5a0b5f3dde790ee9685ca8099
Gunduz E <66138491+Ageslense@users.noreply.github.com>
2022-02-04 00:32:30 +0300

    Add files via upload

--- summary.js
+++ summary.js
@@ -2,45 +2,94 @@ const http = new easyHTTP
 
 let selectedDays = []
 let selectedAddOns = []
+let PNR
 
 const UIremaining = document.querySelector('.remaining-balance')
 const UIform = document.querySelector('.rental-form')
 
-if( JSON.parse(sessionStorage.getItem('days')) == null){
-    document.querySelector('.preloader').classList.remove('active')
-    document.querySelector('.res-summary-page').classList.add('active')
+if( JSON.parse(sessionStorage.getItem('submitted')) == 1){
+    placeholder()
+    
 } else{
 
-let test = JSON.parse(sessionStorage.getItem('days'))
 let form = JSON.parse(sessionStorage.getItem('form'))
 
-console.log(form);
-
-// API POST
-test.forEach( e => {
-
-    let thisDay = {
-        Year: e.year,
-        Month: e.month,
-        Day: e.day,
-        BusinessName: form.find(x => x.name == 'Business-Name').value
-    }
-
-    selectedDays.push(thisDay)
-})
-
-const dayAmount = selectedDays.length
-
-selectedAddOns = form.find( x => x.addOns != undefined).addOns
+placeholder()
 
 http.post('https://api-2adx9.ondigitalocean.app/insert-many', selectedDays, function(e){
 
     document.querySelector('.preloader').classList.remove('active')
     document.querySelector('.res-summary-page').classList.add('active')
 
-    document.querySelector('.pnr-code').textContent = e
-    document.querySelector('.client-name').textContent = form.name
-    document.querySelector('.client-email').textContent = form.email
+    window.sessionStorage.setItem('pnr', e)
+    
+    //Form Submit
+    let formNew = form.filter(x => x.name != undefined)
+    
+    formNew.forEach( e => {
+        console.log(e);
+
+        if(e.name != 'file-2'){
+
+            if(UIform.querySelector(`[name='${e.name}']`).getAttribute('type') == 'radio'){
+    
+                document.querySelectorAll(`[name='${e.name}']`).forEach( a => {
+                    if(e.checked == true){
+                        a.checked = true
+                    }
+                })
+            } else if(UIform.querySelector(`[name='${e.name}']`).getAttribute('type') == 'checkbox'){
+                document.querySelectorAll(`[name='${e.name}']`).forEach( a => {
+                    if(e.checked == true){
+                        a.checked = true
+                    }
+                })
+            } else{
+    
+                console.log(e);
+                if(UIform.querySelector(`[name='${e.name}']`).getAttribute('type') != 'file'){
+                    
+                    UIform.querySelector(`[name='${e.name}']`).value = e.value
+                }
+            }
+        }
+
+
+    })
+    
+    window.sessionStorage.setItem('submitted', 1)
+
+    UIform.querySelector('.reserve-button').click()
+})
+
+}
+
+function placeholder(){
+    let form = JSON.parse(sessionStorage.getItem('form'))
+    let test = JSON.parse(sessionStorage.getItem('days'))
+
+    document.querySelector('.preloader').classList.remove('active')
+    document.querySelector('.res-summary-page').classList.add('active')
+
+    document.querySelector('.pnr-code').textContent = window.sessionStorage.getItem('pnr')
+    document.querySelector('.client-name').textContent = form.find(x => x.name == 'Business-Name').value
+    document.querySelector('.client-email').textContent = form.find(x => x.name == 'Email').value
+    
+    test.forEach( e => {
+
+        let thisDay = {
+            Year: e.year,
+            Month: e.month,
+            Day: e.day,
+            BusinessName: form.find(x => x.name == 'Business-Name').value
+        }
+    
+        selectedDays.push(thisDay)
+    })
+
+    const dayAmount = selectedDays.length
+
+    selectedAddOns = form.find( x => x.addOns != undefined).addOns
 
     const daysWrapper = document.querySelector('[duo="days"]')
     const depositWrapper = document.querySelector('[duo="deposit"]')
@@ -50,11 +99,13 @@ http.post('https://api-2adx9.ondigitalocean.app/insert-many', selectedDays, func
     depositWrapper.innerHTML = ''
     addOnsWrapper.innerHTML = ''
 
+    let dayCost = form.find(x=>x.dayCost != undefined).dayCost
+
     selectedDays.forEach( e => {
 
         let div = document.createElement('div')
 
-        div.innerHTML = `<p class="paragraph-15">${e.Month} ${e.Day}</p><p class="paragraph-15">($${form.dayCost}.00)</p>`
+        div.innerHTML = `<p class="paragraph-15">${e.Month} ${e.Day}</p><p class="paragraph-15">($${dayCost}.00)</p>`
         div.classList.add('reservation-info-duo')
 
         daysWrapper.appendChild(div)
@@ -67,9 +118,8 @@ http.post('https://api-2adx9.ondigitalocean.app/insert-many', selectedDays, func
         depositWrapper.appendChild(divTwo)
     })
 
-    document.querySelector('.total-cost').textContent = `$${form.dayCost*selectedDays.length}.00`
+    document.querySelector('.total-cost').textContent = `$${dayCost*selectedDays.length}.00`
     document.querySelector('.total-deposit').textContent = `$${150*selectedDays.length}.00`
-    UIremaining.textContent = `$${form.dayCost*selectedDays.length - 150*selectedDays.length}.00`
 
     let addOnCost = 0
 
@@ -85,21 +135,7 @@ http.post('https://api-2adx9.ondigitalocean.app/insert-many', selectedDays, func
         addOnsWrapper.appendChild(div)
     })
 
+    UIremaining.textContent = `$${dayCost*selectedDays.length + addOnCost - 150*selectedDays.length}.00`
+
     document.querySelector('.add-on-cost').textContent = `$${addOnCost}.00`
-
-    sessionStorage.removeItem('form')
-    sessionStorage.removeItem('days')
-})
-
-//Form Submit
-let formNew = form.filter(x => x.name != undefined)
-
-formNew.forEach( e => {
-    if(e.type != 'file'){
-
-        UIform.querySelector(`[name='${e.name}']`).value = e.value
-    }
-})
-
-UIform.submit()
 }
